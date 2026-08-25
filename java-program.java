f3a537898353c6ac9c0bc334b00eadb4989f3ab2
Robert Ionut Minea <robertionutminea@Roberts-MacBook-Pro.local>
2026-08-25 16:08:12 +0300

    I've updated the main program

--- java-program.java
+++ java-program.java
@@ -1,5 +1,5 @@
-public class HelloWorld{ 
+public class HelloWorld { 
 	public static void main(String[] args){
-		System.out.println("Hello fmm world");
+		System.out.println("Hello fmm world- made changes");
 	}
 }
